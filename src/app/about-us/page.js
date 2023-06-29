fe747c3854b2d2054ff693993ef177ed2c66a49f
Ikennaf1 <iiikbros202@gmail.com>
2023-06-30 00:39:08 +0100

    About page finish

--- src/app/about-us/page.js
+++ src/app/about-us/page.js
@@ -95,7 +95,7 @@ const AboutUs = () => {
               <Link href="/about-us" className="nav-link"><li>About us</li></Link>
               <Link href="/" className="nav-link"><li>Blog</li></Link>
               <Link href="/" className="nav-link"><li>Products</li></Link>
-              <Link href="/" className="nav-link">
+              <Link href="https://wa.me/message/HFKHEXWNRCEFE1" className="nav-link">
                 <li className="">
                   <Image
                    src="/whatsapp.svg"
@@ -123,21 +123,157 @@ const AboutUs = () => {
       <header className="about-jumbo">
         <div className="pt-32 pb-24 body-margin text-center">
           <h2 className="text-6xl md:text-7xl lg:text-8xl xl:text-9xl text-white font-light lg:font-light drop-shadow-lg uppercase border-b border-white border-dotted inline-block px-8 py-4">Blinds</h2>
-          <p className="mt-4 text-base md:text-lg lg:text-xl xl:text-2xl font-bold text-gray-100 drop-shadow-lg">Helping businesses establish their online businesses.</p>
+          <p className="mt-4 text-base md:text-lg lg:text-xl xl:text-2xl font-bold text-gray-100 drop-shadow-lg">Helping businesses establish their online presence.</p>
         </div>
       </header>
 
       <section>
         <div className="body-margin text-center flex flex-col items-center gap-4 overflow-hidden">
-          <p className="z-10 text-xl md:text-2xl lg:text-3xl font-bold">We are more than just a website<br />development firm</p>
-          <p className="z-10 font-light">We are your partner for establishing an online presence.<br />From humble beginnings as a one-person freelancer, we have grown into a dynamic team of professionals<br />dedicated to crafting exceptional web experiences.</p>
-          <a className="z-10 w-32 mx-auto px-4 py-3 bg-none border-2 border-[#2ec4b6] hover:bg-[#2ec4b6] text-black hover:text-white rounded-3xl ease-linear duration-75 uppercase font-bold text-xs" href="/">Contact us</a>
+          <p className="z-10 text-xl md:text-2xl lg:text-3xl font-bold text-black">We are more than just a website<br />development firm</p>
+          <p className="z-10 font-light">We are your partner in establishing an online presence.<br />We are a humble team of freelancers<br />dedicated to crafting exceptional web experiences for you our client, and your customers.</p>
 
-          <div className="num-backs absolute opacity-20 bg-red-300 flex flex-row mx-auto justify-center items-center text-9xl w-64 h-64 rounded-full font-black">
+          <div className="num-backs absolute opacity-20 bg-red-300 flex flex-row mx-auto justify-center self-start items-center text-9xl w-64 h-64 rounded-full font-black text-[white]">
             <span>01</span>
           </div>
+
+          <Image
+           src="/vertical_line.png"
+           width="4"
+           height="94"
+           alt="vertical line"
+          />
+
+          <p className="z-10 text-xl md:text-2xl lg:text-3xl">Our Mission: Fueling Your Digital Success</p>
+          <p className="z-10 font-light">Our mission at Blinds is simple: to empower businesses and brands in establishing their online presence.<br />
+          We believe that every organization deserves a remarkable website<br />
+          that resonates with their audience and represents their unique identity. Whether it's a sleek corporate site,<br />a captivating e-commerce platform, or an engaging web app, we are here to make your digital aspirations a reality.</p>
+
+          <Image
+           src="/vertical_line.png"
+           width="4"
+           height="94"
+           alt="vertical line"
+          />
+
+          <a className="z-10 w-40 mx-auto px-5 py-4 bg-none border-2 border-[#fb4b7b] hover:bg-[#fb4b7b] text-black hover:text-white hover:shadow-lg rounded-3xl ease-linear duration-100 uppercase font-bold text-xs" href="/">Get started</a>
         </div>
       </section>
+
+      <section className="bg-gray-800 section-2-clip shadow-lg">
+        <div className="body-margin text-center flex flex-col items-center gap-4 overflow-hidden py-16 text-gray-200">
+          <p className="z-10 text-xl md:text-2xl lg:text-3xl font-bold text-white">Core Values</p>
+
+          <Image
+           src="/vertical_line.png"
+           width="4"
+           height="94"
+           alt="vertical line"
+          />
+
+          <div className="flex flex-row flex-wrap gap-16 z-10">
+            <div className="core-values-img core-values-img-1">
+              Professionalism
+            </div>
+            
+            <div className="core-values-img core-values-img-2">
+              Affordability
+            </div>
+
+            <div className="core-values-img core-values-img-3">
+              Exceeding expectations
+            </div>
+          </div>
+
+          <div className="num-backs absolute opacity-20 bg-green-300 flex flex-row mx-auto justify-center self-end items-center text-9xl w-64 h-64 rounded-full font-black text-[white]">
+            <span>02</span>
+          </div>
+
+          <Image
+           src="/vertical_line.png"
+           width="4"
+           height="94"
+           alt="vertical line"
+          />
+
+          <div className="z-10 font-light text-gray-300 md:flex md:flex-row gap-16 md:text-justify">
+            <p className="z-10 mt-16 md:mt-0">We approach each project with utmost professionalism,
+            ensuring clear communication, timely delivery, and meticulous attention to detail.
+            Our commitment to excellence guarantees that your web development experience with us is smooth, efficient, and stress-free.</p>
+
+            <p className="mt-16 md:mt-0">We also understand that budgets vary,
+            especially for startups and small businesses. That is why we offer cost-effective solutions without compromising on quality.
+            Our pricing structures are designed to provide exceptional value for your investment,
+            helping you achieve your online goals within your means.</p>
+
+            <p className="mt-16 md:mt-0">We believe in both satisfying and surpassing your expectations.
+            We are not satisfied with just meeting project requirements; we strive to exceed them.
+            Our team's expertise, dedication, and passion enable us to
+            deliver outstanding results that leave a lasting impact on your audience.</p>
+          </div>
+
+          <Image
+           src="/vertical_line.png"
+           width="4"
+           height="94"
+           alt="vertical line"
+          />
+
+          <a className="z-10 w-40 mx-auto px-5 py-4 bg-none border-2 border-[#2ec4b6] hover:bg-[#2ec4b6] text-white hover:text-white hover:shadow-lg rounded-3xl ease-linear duration-100 uppercase font-bold text-xs" href="/">Contact us</a>
+        </div>
+      </section>
+
+      <section className="section-services">
+        <div className="body-margin text-center flex flex-col items-center gap-4 overflow-hidden">
+          <p className="z-10 text-xl md:text-2xl lg:text-3xl font-bold text-white drop-shadow-lg"><span className="text-gray-300">Expertise and Services: </span>Crafting Exceptional Web Experiences</p>
+
+          {/* <Image
+           src="/vertical_line.png"
+           width="4"
+           height="94"
+           alt="vertical line"
+          /> */}
+
+          <p className="z-10 font-light text-white">At Blinds, we specialize in both back-end and front-end development,<br />
+          ensuring that every aspect of your website<br />
+          functions seamlessly and looks visually stunning.<br />
+          From creating responsive designs and intuitive user interfaces to developing robust functionalities and integrations,<br />
+          we've got you covered.<br/>
+          Our expertise extends to various technologies, frameworks, and platforms,<br />
+          enabling us to tailor solutions that best suit your unique requirements.</p>
+
+          <div className="num-backs absolute opacity-20 bg-[#414eff] flex flex-row mx-auto justify-center self-start items-center text-9xl w-64 h-64 rounded-full font-black text-[white]">
+            <span>03</span>
+          </div>
+
+          {/* <Image
+           src="/vertical_line.png"
+           width="4"
+           height="94"
+           alt="vertical line"
+          /> */}
+
+          <a className="z-10 w-40 mx-auto px-5 py-4 bg-none border-2 border-[#414eff] hover:bg-[#414eff] text-white hover:text-white hover:shadow-lg rounded-3xl ease-linear duration-100 uppercase font-bold text-xs" href="/">Get started</a>
+        </div>
+      </section>
+
+      <section className="bg-gray-500 text-white shadow-lg">
+        <div className="body-margin py-8">
+          <div className="block w-full sm:flex sm:items-center sm:justify-between sm:gap-10">
+              <div className="join-team flex flex-col gap-4 text-center z-10 font-light text-white">
+                  <p className="font-bold drop-shadow-lg">Join the Blinds Family: Let's Shape Your Online Presence</p>
+                  <p className="drop-shadow-lg">We invite you to join the Blinds family and experience the transformative power of exceptional web development. With our unparalleled expertise, cutting-edge technology, and unwavering dedication, we'll help you create a striking online presence that captivates your audience and drives your success.</p>
+                  <p className="drop-shadow-lg">Contact us today to discuss your project, and let's embark on an exciting journey to shape your digital future together.</p>
+                  <a className="z-10 w-40 mx-auto px-5 py-4 bg-none border-2 border-[#cece32] hover:bg-[#cece32] text-white hover:text-black hover:shadow-lg rounded-3xl ease-linear duration-100 uppercase font-bold text-xs" href="/">Get started</a>
+              </div>
+              <div className="section-two-items text-gray-300">
+                <img
+                 src="/team.jpg"
+                 className="object-cover"
+                />
+              </div>
+          </div>
+        </div>
+        </section>
     </main>
   );
 }
